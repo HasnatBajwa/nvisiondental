ca7ee217aa5f0b922f0f92d0dcac6840890fd2bf
HasnatBajwa <hasnatjbajwa@gmail.com>
2023-02-26 13:30:44 +0500

    updated

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Grid} from '@mui/material';
+
+const Nav = () => {
+  return (
+   <>
+   <Grid container justifyContent={'center'}>
+    <Grid>Hello </Grid>
+    <Grid>World</Grid>
+    <Grid>Whats Going On?</Grid>
+   </Grid>
+   </>
+  )
+}
+
+export default Nav
